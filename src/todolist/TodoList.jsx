89ebd7bd94075692933ba56c30e2c288c5cc2cd3
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2025-07-03 11:00:01 +0700

    learn collection component

--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -1,12 +1,48 @@
 import Todo from "./Todo.jsx";
 
 function TodoList() {
+    const data = [
+        { 
+            id: 0,
+            text: "Learn HTML", 
+            isCompleted: true, 
+            isDeleted: true 
+        },
+        { 
+            id: 1,
+            text: "Learn CSS", 
+            isCompleted: true, 
+            isDeleted: false 
+        },
+        { 
+            id: 2,
+            text: "Learn JavaScript", 
+            isCompleted: true, 
+            isDeleted: false 
+        },
+        { 
+            id: 3,
+            text: "Learn React", 
+            isCompleted: false, 
+            isDeleted: false 
+        },
+        { 
+            id: 3,
+            text: "Learn React JS Hooks", 
+            isCompleted: false, 
+            isDeleted: false 
+        }
+    ]
+
+    // const todos = data.map((todo) => {
+    //     return <Todo key={todo.id} {...todo}/>
+    // })
+
     return (
         <ul>
-            <Todo text="Belajar HTML" isCompleted={true} isDeleted={true} />
-            <Todo text="Belajar CSS" isCompleted={true} />
-            <Todo text="Belajar JavaScript" isCompleted={true} />
-            <Todo text="Belajar React" isCompleted={false} />
+            {data.map((todo)=> (
+                <Todo key={todo.id} {...todo} />
+            ))}
         </ul>
     )
 }
