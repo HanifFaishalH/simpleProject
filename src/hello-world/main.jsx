9c65fd6d9e18b3071e62fc74d06c6b91955e3b9a
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2025-07-07 09:57:19 +0700

    learn event propagation

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -6,6 +6,8 @@ import AlertButton from "../button/AlertButton.jsx";
 import MyButton from "../button/myButton.jsx";
 import TodoList from "../todolist/TodoList.jsx";
 import Table from "../table/Table.jsx";
+import Toolbar from "../button/Toolbar.jsx";
+import SearchForm from "../form/SearchForm.jsx";
 
 createRoot(document.querySelector("#root")).render(
     <StrictMode>
@@ -17,6 +19,13 @@ createRoot(document.querySelector("#root")).render(
             <AlertButton text="Click Me" message="hey, you clicked me" />
 
             <MyButton text="Smash Me" onSmash={() => alert("You smashed me!")} />
+
+            <Toolbar onClick={(e) =>{
+                e.stopPropagation()
+                alert("You clicked the toolbar button!");
+            }}/>
+
+            <SearchForm />
         </Container>
     </StrictMode>
 )
