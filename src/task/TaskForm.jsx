7d998f7dc87a2a74ade026d7cca39c732b7d533a
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2025-07-10 19:06:13 +0700

    learn state sharing

---
+++ src/task/TaskForm.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function TaskForm({onSubmit}) {
+    const [item, setItem] = useState("");
+
+    function handleChange(e) {
+        setItem(e.target.value);
+    }
+
+    function handleClick(e) {
+        e.preventDefault();
+        onSubmit(item);
+        setItem("");
+    }
+
+    return (
+        <>
+            <h1>Create Task</h1>
+            <form>
+                <input value={item} onChange={handleChange} />
+                <button onClick={handleClick}>Add</button>
+            </form>
+        </>
+    )
+}
+
+export default TaskForm;
